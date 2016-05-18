aed8f34e78118d3c2e4895521f7c2678d1094240
Santiago Gil <gil.educaciontdf@gmail.com>
2016-05-18 15:38:35 -0300

    Implementa flux y webworker.
    El hecho de separar el negocio de la aplicacion en un webworker, dejando
    libre el UI thread exclusivamente para renderizar, mejora notablemente
    la performance acercandose a la de una app nativa.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,73 @@
+/* Trabajamos el worker mediante webworkify. Esto resulta mas natural que compilar el worker aparte y llamarlo desde index.html. */
+var work = require('webworkify')
+var worker = work(require('./worker.js'))
 var yo = require('yo-yo')
-var lg = require('./modulos/login/login.js')
-var login = lg('')
+
+/* app, es nuestra vista principal. Contiene ademas la logica para elegir la vista en funcion de la url en el estado. */
+var app = require('./app.js')
+
+/* Permite identificar los links locales facilmente y sin lidiar con diferencias entre navegadores. */
+var localLinks = require('local-links')
+
+/* El elemento base sobre el que vamos a correr yo.update */
+var el
+
+/* Por cada evento que recibimos del woker verificamos si existe el elemento base el.
+   Si no existe, vaciamos el body y lo inicializamos.
+   Si existe, ya podemos correr yo.update.
+*/
+worker.onmessage = function (ev) {
+  console.log(ev)
+    var newel = app(ev.data)
+    var url = ev.data.url
+  if (!el) {
+        el = newel
+    document.body.innerHTML = ''
+    return document.body.appendChild(el)
+  }
+  yo.update(el, newel)
+
+/* Si la url de la barra de navegacion no coincide con la recibida, la actualizamos. */
+  if (location.pathname !== url) {
+    history.pushState(null, null, url)
+  }
+}
+
+/* Escuchamos eventos de los botones atras y adelante del navegador y enviamos la nueva url al worker para que actualice el estado */
+window.addEventListener('popstate', function () {
+  worker.postMessage({type: 'setUrl', payload: location.pathname.toString()})
+})
 
 
-var div = yo`<div id="container"><div class="center">${login}</div></div>`
-document.body.appendChild(div)
+// Escuchamos todos los clicks.
+document.body.addEventListener('click', function (event) {
+  // handles internal navigation defined as
+  // clicks on <a> tags that have `href` that is
+  // on the same origin.
+  // https://www.npmjs.com/package/local-links
+  var pathname = localLinks.getLocalPathname(event)
+  if (pathname) {
+    // stop browser from following the link
+    event.preventDefault()
+    // instead, post the new URL to our worker
+    // which will trigger compute a new vDom
+    // based on that new URL state
+    worker.postMessage({type: 'setUrl', payload: pathname})
+    return
+  }
+
+  // this is for other "onClick" type events we want to
+  // respond to. We check existance of an `data-click`
+  // attribute and if it exists, post that back.
+  // In our case, the messages look like either
+  // {type: "increment"}
+  // or
+  // {type: "decrement"}
+  // but could contain any serializable payload
+  // describing the action that occured
+  var click = event.target['data-click']
+  if (click) {
+    event.preventDefault()
+    worker.postMessage(click)
+  }
+})
