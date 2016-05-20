4d3c9f3f9924dabb9ecf7a447708c4c32e62e147
Santiago Gil <gil.educaciontdf@gmail.com>
2016-05-20 15:43:36 -0300

    Habemus server universal!

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
-var express = require('express');  
-var request = require('request');
-var ecstatic = require('ecstatic');
-var httpProxy = require('http-proxy');
+// var express = require('express');  
+// var request = require('request');
+// var ecstatic = require('ecstatic');
+// var httpProxy = require('http-proxy');
+// var fs = require('fs')
 
-var proxy = httpProxy.createProxyServer({});
+// var proxy = httpProxy.createProxyServer({});
 
-var app = express();  
-app.use(ecstatic({ root: __dirname + '/build', handleError: false }))
-app.use('/api', function(req, res) {  
-    proxy.web(req, res, { target: 'http://192.168.1.43' });
-});
+// var app = express();  
+// app.use('/', function (req, res) {return ecstatic({ root: __dirname + '/build', handleError: false })})
+// app.use(ecstatic({ root: __dirname + '/build/assets', handleError: false }))
+// app.use('/api', function(req, res) {  
+//     proxy.web(req, res, { target: 'http://192.168.1.43' });
+// });
 
-app.listen(process.env.PORT || 8081);  
+// app.listen(process.env.PORT || 8081);  
+var fs = require('fs')
+var path = require('path')
+var xtend = require('xtend')
+var hyperstream = require('hyperstream')
+var app = require('./src/app.js')
+
+var ecstatic = require('ecstatic')
+var st = ecstatic(path.join(__dirname, 'build'))
+
+var http = require('http')
+
+var server = http.createServer(function (req, res) {
+  var state = {
+  url: '',
+  title: "El titulo",
+  tabs: [
+  {title: "Primer titulo", content: "Primer contenido", activeTab: true},
+  {title: "Segundo titulo", content: "Segundo contenido"},
+  {title: "tercer titulo", content: "Segundo contenido"},
+  {title: "cuarto titulo", content: "Segundo contenido"},
+  {title: "cuarto titulo", content: "Segundo contenido"},
+  {title: "cuarto titulo", content: "Segundo contenido"}
+  ]
+}
+  if (!app(xtend(state, {url: req.url}))) return st(req, res)
+    var elem =  app(xtend(state, {url: req.url}))
+  read('index.html').pipe(hyperstream({
+      '#content': elem.toString()
+    })).pipe(res)
+})
+server.listen(8000)
+
+function read (x) {
+  return fs.createReadStream(path.join(__dirname, 'build', x))
+}
--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ var panel = require('./panel.js')
 var yo = require('yo-yo')
 
 module.exports = function (state) {
-  if (state.url === '/login') return login(state)
+  if (state.url === '/') return yo`<div><a href='/login'>login</a> <a href='/panel'>panel</a></div>`
+  else if (state.url === '/login') return login(state)
   else if (state.url === '/panel') return panel(state)
-  else return yo`<div><a href='/login'>login</a> <a href='/panel'>panel</a></div>`
+  else return undefined
 }
